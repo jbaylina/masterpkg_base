17a9c9fcad51029678a093fbcc198ea55c05cd8e
Francesc Jimenez <francesc@masterasp.com>
2015-10-16 13:06:31 +0200

    Añadir server al __mods para poder crear el socket.io

--- master_modules/my_ang_boot/server/index.js
+++ master_modules/my_ang_boot/server/index.js
@@ -37,8 +37,10 @@ var fs = require("fs");
 var db = __mods.db = require('./db');
 __mods.MasterError = require('../common/mastererror');
 
-
 var app = __mods.app =  express();
+
+var server = http.createServer(app);
+__mods.server = server;
 
 db.on('init', function() {
 	app.set('port', config.port || 3000);
@@ -200,33 +202,7 @@ db.on('init', function() {
 		res.json(errObj);
 	});
 
-	/***********************************
-	//			CONFIG SOCKET.IO
-	***********************************
-	var server = http.createServer(app);
-	if(config.socket){
-		var io = require('socket.io')(server);
-		var clicks = 0;
-		io.on('connection', function(socket){
-			console.log('Socket io connected (Id: '+socket.id+')');
-			/*
-			socket hauria de ser "global", per podero configurar a cada módul que ho necesiti
-			* /
-			socket.emit('connected', true);
-		    socket.on('clicks', function(data) {
-			    clicks++;
-			    io.sockets.emit('clicks', clicks);
-		  	});
-		});
-	}
-	/*
-		A banda d'aquest if, tmb he modificat el "createServer" per guardaro a la variable "server"
-	*/
-	/***********************************
-	//			CONFIG SOCKET.IO
-	***********************************/
-
-	http.createServer(app).listen(app.get('port'), function () {
+	server.listen(app.get('port'), function () {
 		console.log('Express server listening on port ' + app.get('port'));
 		logger.log('verbose', 'Express server listening on port ' + app.get('port'));
 	});
