66b5dfde3f978d94064935d9aaecdaa77b1099e9
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2016-02-16 12:05:53 +0100

    ckeditor with upload and select images from server, rsv4_bookings with MEDIUMTEXT on bookingData

--- master_modules/ckeditor/client/ckeditor.js
+++ master_modules/ckeditor/client/ckeditor.js
@@ -7,92 +7,103 @@
 
     var mod = angular.module('ckeditor', []);
 
-    mod.directive('ckEditor', ["$timeout", function ($timeout) {
-        return {
-            require: '?ngModel',
-            link   : function (scope, elm, attr, ngModel) {
-                CKEDITOR.config.protectedSource = [/<%.*%>/g, /&nbsp;/g, /<>/g];
-                var ck                          = CKEDITOR.replace(elm[0], {
-                    extraPlugins: 'justify,colorbutton,font',
-                    toolbar     : [
-                        {
-                            name : 'clipboard',
-                            items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']
-                        },
-                        {
-                            name : 'editing',
-                            items: ['Find', 'Replace', '-', 'SpellChecker', 'Scayt']
-                        },
-                        {
-                            name : 'forms',
-                            items: []
-                        },
-                        {
-                            name : 'links',
-                            items: []
-                        },
-                        {
-                            name : 'insert',
-                            items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Link']
-                        },
-                        {
-                            name : 'tools',
-                            items: ['Maximize']
-                        },
-                        {
-                            name : 'document',
-                            items: ['Source', '-', 'NewPage', 'Preview', '-', 'Templates']
-                        },
-                        '/',
-                        {
-                            name : 'styles',
-                            items: ['Format', 'Font', 'FontSize']
-                        },
-                        {
-                            name : 'basicstyles',
-                            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'basicstyles', 'cleanup']
-                        },
-                        {
-                            name : 'paragraph',
-                            items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
-                        },
-                        {
-                            name : 'colors',
-                            items: ['TextColor', 'BGColor']
-                        }
-                    ],
-                    height      : '290px',
-                    width       : '99%'
-                });
+    mod.directive('ckEditor', ["$timeout", "clientConfig",
+        function ($timeout, clientConfig) {
+            return {
+                require: '?ngModel',
+                link: function (scope, elm, attr, ngModel) {
+                    CKEDITOR.config.protectedSource = [/<%.*%>/g, /&nbsp;/g, /<>/g];
+                    CKEDITOR.config.allowedContent = true;
 
-                if (!ngModel) {
-                    return;
-                }
+                    if(clientConfig.ckeditor.filebrowserUploadUrl) {
+                        CKEDITOR.config.filebrowserUploadUrl = clientConfig.ckeditor.filebrowserUploadUrl;
+                    }
 
-                //loaded didn't seem to work, but instanceReady did
-                //I added this because sometimes $render would call setData before the ckeditor was ready
-                ck.on('instanceReady', function () {
-                    ck.setData(ngModel.$viewValue);
-                });
+                    if(clientConfig.ckeditor.filebrowserBrowseUrl) {
+                        CKEDITOR.config.filebrowserBrowseUrl = clientConfig.ckeditor.filebrowserBrowseUrl;
+                    }
 
-                ck.on('pasteState', function () {
-                    scope.$apply(function () {
-                        ngModel.$setViewValue(ck.getData());
+                    var ck = CKEDITOR.replace(elm[0], {
+                        extraPlugins: 'justify,colorbutton,font',
+                        toolbar: [
+                            {
+                                name: 'clipboard',
+                                items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']
+                            },
+                            {
+                                name: 'editing',
+                                items: ['Find', 'Replace', '-', 'SpellChecker', 'Scayt']
+                            },
+                            {
+                                name: 'forms',
+                                items: []
+                            },
+                            {
+                                name: 'links',
+                                items: []
+                            },
+                            {
+                                name: 'insert',
+                                items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Link']
+                            },
+                            {
+                                name: 'tools',
+                                items: ['Maximize']
+                            },
+                            {
+                                name: 'document',
+                                items: ['Source', '-', 'NewPage', 'Preview', '-', 'Templates']
+                            },
+                            '/',
+                            {
+                                name: 'styles',
+                                items: ['Format', 'Font', 'FontSize']
+                            },
+                            {
+                                name: 'basicstyles',
+                                items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'basicstyles', 'cleanup']
+                            },
+                            {
+                                name: 'paragraph',
+                                items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+                            },
+                            {
+                                name: 'colors',
+                                items: ['TextColor', 'BGColor']
+                            }
+                        ],
+                        height: '290px',
+                        width: '99%'
                     });
-                });
 
-                ck.on('key',
-                    function () {
-                        $timeout(function () {
+                    if (!ngModel) {
+                        return;
+                    }
+
+                    //loaded didn't seem to work, but instanceReady did
+                    //I added this because sometimes $render would call setData before the ckeditor was ready
+                    ck.on('instanceReady', function () {
+                        ck.setData(ngModel.$viewValue);
+                    });
+
+                    ck.on('pasteState', function () {
+                        scope.$apply(function () {
                             ngModel.$setViewValue(ck.getData());
                         });
                     });
 
-                ngModel.$render = function () {
-                    ck.setData(ngModel.$viewValue);
-                };
+                    ck.on('key',
+                        function () {
+                            $timeout(function () {
+                                ngModel.$setViewValue(ck.getData());
+                            });
+                        });
 
-            }
-        };
-    }]);
+                    ngModel.$render = function () {
+                        ck.setData(ngModel.$viewValue);
+                    };
+
+                }
+            };
+        }]);
 })();
