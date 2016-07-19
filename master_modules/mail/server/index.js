0d7cd0af92ec78e98de48363f4cdc74cc4d1f7e6
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2016-07-19 13:20:29 +0200

    generic mail bug fixed

--- master_modules/mail/server/index.js
+++ master_modules/mail/server/index.js
@@ -79,15 +79,15 @@ function setMailConfig(config, callback){
  */
 function mail(mailOptions, callback){
 
-    if(!serverMailConfig) return callback("Mail config not defined");
+    if(!serverMailConfig && callback) return callback("Mail config not defined");
 
     transporter.sendMail(mailOptions, function (errMail) {
         if (errMail) {
-            log("error", "error sending generic mail", errMail);
-            callback(errMail);
+            log("warn", "error sending generic mail", errMail);
+            if(callback) callback(errMail);
         } else {
             log("silly", "mail sended to " + mailOptions.to);
-            callback();
+            if(callback) callback();
         }
     });
 
