f75fe6c7eaa92d3377377119d99a875a76c705c0
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2017-01-13 12:20:22 +0100

    iCheck directive added

--- master_modules/template_admin/client/template.js
+++ master_modules/template_admin/client/template.js
@@ -204,6 +204,43 @@
         };
     }
 
+    /**
+     * icheck - Directive for custom checkbox icheck
+     */
+    function icheck($timeout) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function($scope, element, $attrs, ngModel) {
+                return $timeout(function() {
+                    var value;
+                    value = $attrs['value'];
+
+                    $scope.$watch($attrs['ngModel'], function(newValue){
+                        $(element).iCheck('update');
+                    })
+
+                    return $(element).iCheck({
+                        checkboxClass: 'icheckbox_square-green',
+                        radioClass: 'iradio_square-green'
+
+                    }).on('ifChanged', function(event) {
+                        if ($(element).attr('type') === 'checkbox' && $attrs['ngModel']) {
+                            $scope.$apply(function() {
+                                return ngModel.$setViewValue(event.target.checked);
+                            });
+                        }
+                        if ($(element).attr('type') === 'radio' && $attrs['ngModel']) {
+                            return $scope.$apply(function() {
+                                return ngModel.$setViewValue(value);
+                            });
+                        }
+                    });
+                });
+            }
+        };
+    }
+
 
     function config($stateProvider, IdleProvider) {
 
@@ -245,6 +282,7 @@
         .directive('pageTitle', pageTitle)
         .directive('sideNavigation', sideNavigation)
         .directive('iboxTools', iboxTools)
+        .directive('icheck', icheck)
         .directive('minimalizaSidebar', minimalizaSidebar)
         .directive('iboxToolsFullScreen', iboxToolsFullScreen)
         .config(config)
