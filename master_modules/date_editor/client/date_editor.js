0032cd2e3acef844b8ed8975d4a763cfcc097722
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2017-02-27 11:33:53 +0100

    dateEditor directive bug with max and min date fixed

--- master_modules/date_editor/client/date_editor.js
+++ master_modules/date_editor/client/date_editor.js
@@ -22,16 +22,8 @@
                     ' type="text" ' +
                     ' ng-model="ngModel" ';
 
-                if (attrs.$attr.minDate) {
-                    tmpTemplate += ' min-date="minDate" ';
-                }
-
-                if (attrs.$attr.maxDate) {
-                    tmpTemplate += ' max-date="maxDate" ';
-                }
-
                 if (attrs.$attr.ngRequired) {
-                    tmpTemplate += ' ng-required="true" ';
+                    tmpTemplate += ' ng-required="ngRequired" ';
                 }
 
                 tmpTemplate += ' uib-datepicker-popup="{{format}}" ' +
@@ -49,6 +41,7 @@
                 return tmpTemplate;
             },
             link: function ($scope, elm, attr) {
+
                 $scope.status = {
                     opened: false
                 };
@@ -65,8 +58,18 @@
 
                 $scope.dateOptions = {
                     formatYear: 'yyyy',
-                    startingDay: 1
+                    startingDay: 1,
+                    minDate: $scope.minDate,
+                    maxDate: $scope.maxDate
                 };
+
+                $scope.$watch('minDate', function(){
+                    if($scope.minDate) $scope.dateOptions.minDate = $scope.minDate;
+                });
+
+                $scope.$watch('maxDate', function(){
+                    if($scope.maxDate) $scope.dateOptions.maxDate = $scope.maxDate;
+                });
 
                 $scope.open = function ($event) {
                     $event.preventDefault();
@@ -86,8 +89,6 @@
                         return S;
                     }
 
-                    //console.log("before parser: " + typeof(viewValue) + " -> " + viewValue);
-
                     viewValue = new Date(viewValue);
                     var y = viewValue.getFullYear();
                     var m = viewValue.getMonth() + 1;
@@ -95,21 +96,16 @@
 
                     viewValue = new Date(fill(y, 4) + '-' + fill(m, 2) + '-' + fill(d, 2) + 'T00:00:00.000Z');
 
-                    //console.log("after parser: " + typeof(viewValue) + " -> " + viewValue);
-
                     return viewValue;
                 });
 
                 // called with a 'yyyy-mm-dd' string to format
                 ngModelController.$formatters.push(function (modelValue) {
-                    // console.log("before formater: " + typeof(modelValue) + " -> " + modelValue);
 
                     if (modelValue) {
                         modelValue = new Date(modelValue);
                         modelValue = new Date(modelValue.toISOString().substr(0, 10));
                     }
-
-                    // console.log("after formater: " + typeof(modelValue) + " -> " + modelValue);
 
                     return modelValue;
                 });
