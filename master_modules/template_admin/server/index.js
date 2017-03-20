96e9c9d56975ff9fbd7a90fb49ccd6ed1251fd32
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2017-03-20 15:16:37 +0100

    Disassociate logo of master camping project

--- master_modules/template_admin/server/index.js
+++ master_modules/template_admin/server/index.js
@@ -8,19 +8,16 @@ var fs = require("fs-extra");
 var config = __mods.config;
 
 exports.init = function () {
-    if (config.clientConfig.property && config.clientConfig.property.logo) {
-        var logo = config.clientConfig.property.logo || "/media/sf_mastersq/Bookings/logo.png";
-        fs.stat(logo, function (err, stat) {
+    if (config.clientConfig.logo) {
+        fs.stat(config.clientConfig.logo, function (err, stat) {
             if (err == null) {
                 var inStr = fs.createReadStream(config.clientConfig.property.logo);
                 var outStr = fs.createWriteStream("/git/masterweb/dist/images/logo_blanc.png");
                 inStr.pipe(outStr);
             } else {
-                console.warn((config.clientConfig.property.logo || logo) + " not exists!");
+                console.warn((config.clientConfig.property.logo) + " not exists!");
                 console.error(err);
             }
         });
-    } else {
-        console.error("config.clientConfig.property not defined");
     }
 };
